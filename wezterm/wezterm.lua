44a56962be5f8804f9908179d558c39e7b2f14c8
Mukize <patrickmukize@gmail.com>
2024-07-02 17:51:06 +0200

    Wezterm changes for ssh

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,6 +9,7 @@ config.line_height = 0.9
 ---
 
 --- appearance
+config.audible_bell = "Disabled"
 config.color_scheme = "Tokyo Night"
 config.window_background_opacity = 0.9
 config.window_decorations = "RESIZE"
@@ -21,6 +22,7 @@ config.window_padding = {
 ---
 
 --- tab bar
+config.hide_tab_bar_if_only_one_tab = true
 config.tab_max_width = 50
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
